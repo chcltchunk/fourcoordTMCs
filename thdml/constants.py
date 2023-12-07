8c1b887c45eb72c61bbec4ea80f2b0d133f38750
Jonas <joldenstaedt@gmail.com>
2023-12-07 09:57:56 +0100

    added comments

--- thdml/constants.py
+++ thdml/constants.py
@@ -5,5 +5,6 @@ SSE_CUTOFF = -110
 
 
 # define properties we'd like to keep in the dataset throughout the complete workflow
+# TODO(ralf): should we have the formal charge in the final dataset?
 column_list = ['metal', 'ox', 'ligstr', 'complex.size']  # , 'charge'
 sse_colum_list = ['geom.ls', 'geom.hs', 'ls.spin', 'hs.spin', 'b3lyp.energy.ls (Ha)', 'b3lyp.energy.hs (Ha)', 'b3lyp.sse (kcal/mol)']
--- thdml/data_cleanup.py
+++ thdml/data_cleanup.py
@@ -8,15 +8,17 @@ raw_data_dir = "../data/"
 df_hetero = pd.read_csv(raw_data_dir + "heteroleptic_thd_sses_bl_homo_with_validation_data_exchange_sensitivity.ssv", sep=";")
 df_homo = pd.read_csv(raw_data_dir + "homoleptic_thd_sses_bl_homo_with_validation_data_exchange_sensitivity.ssv", sep=";")
 
-# TODO(ralf): should we have charge in the final dataset?
-# select relevant properties from dataset
-# TODO(jonas): remove up until pd.to_csv and pd.load_csv instead
+# combine the two raw datasets
 df = pd.concat([df_homo, df_hetero])
 
+# remove everything where neither LS or HS is tetrahedral or square planar
 df = df[((df["geom.hs"] == "tetrahedral") | (df["geom.hs"] == "square planar")) | ((df["geom.ls"] == "tetrahedral") | (df["geom.ls"] == "square planar"))]
 # TODO(ralf): I'd remove any detailed results for <S2> from the dataset before preparing it for the ML tasks?
+# otherwise we can move this to the data_preparation script
 df = df[((np.abs(df["s2_is.hs"] - df["s2_expect.hs"])) <= S2_CUTOFF)]
 
+# mask only the relevant property columns from the dataset
 df = df[[*column_list, *sse_colum_list]]
 
+# store dataset that is supplied with SI
 df.to_csv(raw_data_dir + "thd_tmcs_geom_sse.csv")
