b42f527f4cdb5bfaaf3a08de89f027565cd4892b
Jonas <joldenstaedt@gmail.com>
2023-12-08 15:49:47 +0100

    CFF mostly working; 1 test failing

--- fcTMCml/featurizer/CFF.py
+++ fcTMCml/featurizer/CFF.py
@@ -25,27 +25,19 @@
 # Imports
 # =============================================================================
 import numpy as np
-from fcTMCml.constants import EPS, deg_sqp, dqs_sqp, deg_thd, dqs_thd
+from fcTMCml.constants import EPS
 
 
 class CrystalFieldFeatures():
-    def __init__(self, metal, ox, mult, lig_list=None, EPS=1e-6):
+    def __init__(self, metal, ox, mult):
         self.metal = metal.lower()
         self.ox = int(ox)
         self.mult = int(mult)
-        self.lig_list = lig_list
         self.init_dictionaries()
         
 
 
     def init_dictionaries(self): 
-        # TODO add back 
-        # self.cores_d_conf = {"co" : [7, 6],
-        #          "ni" : [8],
-        #          "cr" : [4, 3],
-        #          "fe" : [6, 5],
-        #          "mn" : [5, 4]
-        #             }
         self.cores_d_conf = {"co" : {2: 7, 3 : 6},
                  "ni" : {2 : 8},
                  "cr" : {2 : 4, 3 : 3},
@@ -53,23 +45,37 @@ class CrystalFieldFeatures():
                  "mn" : {2 : 5, 3 : 4}
                     }
         
-        # define geometry based degeneracy patterns from and dq energy schemes
+        # define geometry based degeneracy patterns
         self.geometry_degeneracies = {
             "square planar" : [2, 1, 1, 1],
             "tetrahedral" : [2, 3]
         }
+        # define geometry based differential of quanta energy schemes
         self.geometry_diff_of_quanta = {
             "tetrahedral" : np.array([-2.67, -2.67, 1.78, 1.78, 1.78]),
             "square planar" : np.array([-5.14, -5.14, -4.28, 2.28, 12.28])
         }
 
-
-
-
-
-    def calculate_enes(self, occ, dqs):  
+    def calculate_enes(self, occ, dqs):
+        print(occ)
+        print(dqs) 
         conf_ene = np.dot(occ, dqs)
         return conf_ene
+
+    # TODO: what was that used for?
+    def calculate_comb_factors(self, occupation, deg):
+        occupation = np.array(occupation)
+        comb_factors = [0] * len(deg)
+        ind = [0] + deg
+        ind = np.cumsum(ind)
+        for i in range(1, len(ind)):
+            # print("occup: ", occupation[ind[i-1]:ind[i]])
+            # print("indices", ind[i-1], ind[i])
+            alpha_curr_deg_level = occupation[ind[i-1]:ind[i]] == 1
+            # print("n_alpha: ", alpha_curr_deg_level)
+            comb_factors[i-1] = np.sum(alpha_curr_deg_level.astype(int))
+        #print("comb facs: ", comb_factors)
+        return np.array(comb_factors)
 
 
     def occupy_d_orbitals(self):
@@ -82,7 +88,6 @@ class CrystalFieldFeatures():
                 raise "electron config and multiplicity do not match"
         double_occ = (num_es - single_es)//2
         occ = [0]*5
-        self.mult
         for i in range(double_occ):
             occ[i] = 2
         for i in range(double_occ, double_occ+single_es):
@@ -91,77 +96,71 @@ class CrystalFieldFeatures():
 
     def get_energy_diff_sqp_thd(self):
         occ = self.occupy_d_orbitals()
-        thd_ene = self.calculate_enes(occ, self.thd_dqs)
-        sqp_ene = self.calculate_enes(occ, self.sqp_dqs)
+        thd_ene = self.calculate_enes(occ, self.geometry_diff_of_quanta["tetrahedral"])
+        sqp_ene = self.calculate_enes(occ, self.geometry_diff_of_quanta["square planar"])
         return thd_ene-sqp_ene
 
-    def get_energy_diff_sqp_thd_and_geom_guess(self, dbloc=False):
-        enediff = self.get_energy_diff_sqp_thd(dbloc)
+    def get_energy_diff_sqp_thd_and_geom_guess(self):
+        enediff = self.get_energy_diff_sqp_thd()
         guess = 0 if enediff < 0 else 1 
         return enediff, guess
     
         
 
 
-dbloc = CrystalFieldFeatures("cr", 3, 4, [])
-assert np.all(dbloc.occupy_d_orbitals() == [1,1,1,0,0])
-dbloc = CrystalFieldFeatures("mn", 3, 5, [])
-assert np.all(dbloc.occupy_d_orbitals() == [1,1,1,1,0])
-dbloc = CrystalFieldFeatures("mn", 2, 6, [])
-assert np.all(dbloc.occupy_d_orbitals() == [1,1,1,1,1])
-dbloc = CrystalFieldFeatures("fe", 3, 4, [])
-assert np.all(dbloc.occupy_d_orbitals() == [2,1,1,1,0])
-dbloc = CrystalFieldFeatures("mn", 2, 2, [])
-assert np.all(dbloc.occupy_d_orbitals() == [2,2,1,0,0])
-dbloc = CrystalFieldFeatures("fe", 2, 1, [])
-assert np.all(dbloc.occupy_d_orbitals() == [2,2,2,0,0])
-dbloc = CrystalFieldFeatures("co", 2, 2, [])
-assert np.all(dbloc.occupy_d_orbitals() == [2,2,2,1,0])
-dbloc = CrystalFieldFeatures("ni", 2, 1, [])
-assert np.all(dbloc.occupy_d_orbitals() == [2,2,2,2,0])
-dbloc = CrystalFieldFeatures("ni", 2, 3, [])
-assert np.all(dbloc.occupy_d_orbitals() == [2,2,2,1,1])
+cff = CrystalFieldFeatures("cr", 3, 4)
+assert np.all(cff.occupy_d_orbitals() == [1,1,1,0,0])
+cff = CrystalFieldFeatures("mn", 3, 5)
+assert np.all(cff.occupy_d_orbitals() == [1,1,1,1,0])
+cff = CrystalFieldFeatures("mn", 2, 6)
+assert np.all(cff.occupy_d_orbitals() == [1,1,1,1,1])
+cff = CrystalFieldFeatures("fe", 3, 4)
+assert np.all(cff.occupy_d_orbitals() == [2,1,1,1,0])
+cff = CrystalFieldFeatures("mn", 2, 2)
+assert np.all(cff.occupy_d_orbitals() == [2,2,1,0,0])
+cff = CrystalFieldFeatures("fe", 2, 1)
+assert np.all(cff.occupy_d_orbitals() == [2,2,2,0,0])
+cff = CrystalFieldFeatures("co", 2, 2)
+assert np.all(cff.occupy_d_orbitals() == [2,2,2,1,0])
+cff = CrystalFieldFeatures("ni", 2, 1)
+assert np.all(cff.occupy_d_orbitals() == [2,2,2,2,0])
+cff = CrystalFieldFeatures("ni", 2, 3)
+assert np.all(cff.occupy_d_orbitals() == [2,2,2,1,1])
 
 
-dbloc = CrystalFieldFeatures("cr", 3, 4, [])
-assert np.all(dbloc.calculate_comb_factors([1,0,0,0,0], dbloc.thd_deg) == [1, 0])
-assert np.all(dbloc.calculate_comb_factors([1,1,0,0,0], dbloc.thd_deg) == [2, 0])
-assert np.all(dbloc.calculate_comb_factors([1,1,1,0,0], dbloc.thd_deg) == [2, 1])
-assert np.all(dbloc.calculate_comb_factors([1,1,1,1,0], dbloc.thd_deg) == [2, 2])
-assert np.all(dbloc.calculate_comb_factors([1,1,1,1,1], dbloc.thd_deg) == [2, 3])
-assert np.all(dbloc.calculate_comb_factors([2,1,1,1,1], dbloc.thd_deg) == [1, 3])
-assert np.all(dbloc.calculate_comb_factors([2,2,1,1,1], dbloc.thd_deg) == [0, 3])
-assert np.all(dbloc.calculate_comb_factors([2,2,2,1,1], dbloc.thd_deg) == [0, 2])
-assert np.all(dbloc.calculate_comb_factors([2,2,1,1,1], dbloc.sqp_deg) == [0, 1, 1, 1])
+cff = CrystalFieldFeatures("cr", 3, 4)
+assert np.all(cff.calculate_comb_factors([1,0,0,0,0], cff.geometry_degeneracies["tetrahedral"]) == [1, 0])
+assert np.all(cff.calculate_comb_factors([1,1,0,0,0], cff.geometry_degeneracies["tetrahedral"]) == [2, 0])
+assert np.all(cff.calculate_comb_factors([1,1,1,0,0], cff.geometry_degeneracies["tetrahedral"]) == [2, 1])
+assert np.all(cff.calculate_comb_factors([1,1,1,1,0], cff.geometry_degeneracies["tetrahedral"]) == [2, 2])
+assert np.all(cff.calculate_comb_factors([1,1,1,1,1], cff.geometry_degeneracies["tetrahedral"]) == [2, 3])
+assert np.all(cff.calculate_comb_factors([2,1,1,1,1], cff.geometry_degeneracies["tetrahedral"]) == [1, 3])
+assert np.all(cff.calculate_comb_factors([2,2,1,1,1], cff.geometry_degeneracies["tetrahedral"]) == [0, 3])
+assert np.all(cff.calculate_comb_factors([2,2,2,1,1], cff.geometry_degeneracies["tetrahedral"]) == [0, 2])
+assert np.all(cff.calculate_comb_factors([2,2,1,1,1], cff.geometry_degeneracies["square planar"]) == [0, 1, 1, 1])
 
 
-
-
-
-
-
-EPS = 1e-6
-dbloc = CrystalFieldFeatures("cr", 3, 4, None)
-occ = dbloc.occupy_d_orbitals()
-assert abs(dbloc.calculate_enes(occ, dbloc.thd_dqs) - (-3.56)) <= EPS 
-dbloc = CrystalFieldFeatures("ni", 2, 3, ["test_dummy"])
-occ = dbloc.occupy_d_orbitals()
-assert abs(dbloc.calculate_enes(occ, dbloc.sqp_dqs, False) - (-14.56)) <= EPS 
-dbloc = CrystalFieldFeatures("mn", 3, 5, None)
-occ = dbloc.occupy_d_orbitals()
-assert abs(dbloc.calculate_enes(occ, dbloc.sqp_dqs, False) - (-12.28)) <= EPS 
-dbloc = CrystalFieldFeatures("cr", 3, 2, None)
-occ = dbloc.occupy_d_orbitals()
-assert abs(dbloc.calculate_enes(occ, dbloc.thd_dqs) - (-8.01)) <= EPS 
-dbloc = CrystalFieldFeatures("fe", 2, 1, [])
-occ = dbloc.occupy_d_orbitals()
-assert abs(dbloc.calculate_enes(occ, dbloc.thd_dqs, False) - (-7.12)) <= EPS 
-dbloc = CrystalFieldFeatures("ni", 2, 1, [])
-occ = dbloc.occupy_d_orbitals()
-assert abs(dbloc.calculate_enes(occ, dbloc.sqp_dqs, False) - (-24.56)) <= EPS 
-dbloc = CrystalFieldFeatures("cr", 2, 1, [])
-occ = dbloc.occupy_d_orbitals()
-assert abs(dbloc.calculate_enes(occ, dbloc.sqp_dqs, False) - (-20.56)) <= EPS 
+cff = CrystalFieldFeatures("cr", 3, 4)
+occ = cff.occupy_d_orbitals()
+assert abs(cff.calculate_enes(occ, cff.geometry_diff_of_quanta["tetrahedral"]) - (-3.56)) <= EPS 
+cff = CrystalFieldFeatures("ni", 2, 3)
+occ = cff.occupy_d_orbitals()
+assert abs(cff.calculate_enes(occ, cff.geometry_diff_of_quanta["square planar"]) - (-14.56)) <= EPS 
+cff = CrystalFieldFeatures("mn", 3, 5)
+occ = cff.occupy_d_orbitals()
+assert abs(cff.calculate_enes(occ, cff.geometry_diff_of_quanta["square planar"]) - (-12.28)) <= EPS 
+cff = CrystalFieldFeatures("cr", 3, 2)
+occ = cff.occupy_d_orbitals()
+assert abs(cff.calculate_enes(occ, cff.geometry_diff_of_quanta["tetrahedral"]) - (-8.01)) <= EPS 
+cff = CrystalFieldFeatures("fe", 2, 1)
+occ = cff.occupy_d_orbitals()
+assert abs(cff.calculate_enes(occ, cff.geometry_diff_of_quanta["tetrahedral"]) - (-7.12)) <= EPS 
+cff = CrystalFieldFeatures("ni", 2, 1)
+occ = cff.occupy_d_orbitals()
+assert abs(cff.calculate_enes(occ, cff.geometry_degeneracies["square planar"]) - (-24.56)) <= EPS 
+cff = CrystalFieldFeatures("cr", 2, 1)
+occ = cff.occupy_d_orbitals()
+assert abs(cff.calculate_enes(occ, cff.geometry_degeneracies["square planar"]) - (-20.56)) <= EPS 
 
 
 
@@ -193,13 +192,13 @@ def extend_racs(
         if extension_type == 'default':
             extensions += [[ox, mult, *dents]]
         elif extension_type == 'phys_ediffs':
-            dbloc = CrystalFieldFeatures(metal, ox, mult, None)
+            dbloc = CrystalFieldFeatures(metal, ox, mult)
             occ = dbloc.occupy_d_orbitals()
             #ediff = dbloc.get_energy_diff_sqp_thd(dbloc=False)
             ediff, guess = dbloc.get_energy_diff_sqp_thd_and_geom_guess(dbloc=False)
             extensions += [[ox, ediff, guess, *lig_charges, *dents]]
         elif extension_type == 'phys_ediffs_only':
-            dbloc = CrystalFieldFeatures(metal, ox, mult, None)
+            dbloc = CrystalFieldFeatures(metal, ox, mult)
             occ = dbloc.occupy_d_orbitals()
             #ediff = dbloc.get_energy_diff_sqp_thd(dbloc=False)
             ediff, guess = dbloc.get_energy_diff_sqp_thd_and_geom_guess(dbloc=False)
@@ -243,7 +242,7 @@ def extend_mcdl46(
             dents += [1 if (type(dent_info) == str) else len(dent_info)]
             lig_charges += [int(molsimplify_ligand_dict[lig][5][0])]
         if extension_type == 'phys_ediffs':
-            dbloc = CrystalFieldFeatures(metal, ox, mult, None)
+            dbloc = CrystalFieldFeatures(metal, ox, mult)
             occ = dbloc.occupy_d_orbitals()
             #ediff = dbloc.get_energy_diff_sqp_thd(dbloc=False)
             ediff, guess = dbloc.get_energy_diff_sqp_thd_and_geom_guess(dbloc=False)
