ebf968ae5feb0ab3355b0dafbac0586ca4ffcd8e
Jonas <joldenstaedt@gmail.com>
2023-12-10 21:06:25 +0100

    fixed flake8 in test_featurizer.py

--- tests/test_featurizer.py
+++ tests/test_featurizer.py
@@ -6,51 +6,52 @@ from fcTMCml.featurizer.CFF import CrystalFieldFeatures
 def test_CFF_d_orbital_occupation():
     cff = CrystalFieldFeatures("cr", 3, 4)
     cff.occupy_d_orbitals()
-    assert np.all(cff.occ == [1,1,1,0,0])
+    assert np.all(cff.occ == [1, 1, 1, 0, 0])
     cff = CrystalFieldFeatures("mn", 3, 5)
     cff.occupy_d_orbitals()
-    assert np.all(cff.occ == [1,1,1,1,0])
+    assert np.all(cff.occ == [1, 1, 1, 1, 0])
     cff = CrystalFieldFeatures("mn", 2, 6)
     cff.occupy_d_orbitals()
-    assert np.all(cff.occ == [1,1,1,1,1])
+    assert np.all(cff.occ == [1, 1, 1, 1, 1])
     cff = CrystalFieldFeatures("fe", 3, 4)
     cff.occupy_d_orbitals()
-    assert np.all(cff.occ == [2,1,1,1,0])
+    assert np.all(cff.occ == [2, 1, 1, 1, 0])
     cff = CrystalFieldFeatures("mn", 2, 2)
     cff.occupy_d_orbitals()
-    assert np.all(cff.occ == [2,2,1,0,0])
+    assert np.all(cff.occ == [2, 2, 1, 0, 0])
     cff = CrystalFieldFeatures("fe", 2, 1)
     cff.occupy_d_orbitals()
-    assert np.all(cff.occ == [2,2,2,0,0])
+    assert np.all(cff.occ == [2, 2, 2, 0, 0])
     cff = CrystalFieldFeatures("co", 2, 2)
     cff.occupy_d_orbitals()
-    assert np.all(cff.occ == [2,2,2,1,0])
+    assert np.all(cff.occ == [2, 2, 2, 1, 0])
     cff = CrystalFieldFeatures("ni", 2, 1)
     cff.occupy_d_orbitals()
-    assert np.all(cff.occ == [2,2,2,2,0])
+    assert np.all(cff.occ == [2, 2, 2, 2, 0])
     cff = CrystalFieldFeatures("ni", 2, 3)
     cff.occupy_d_orbitals()
-    assert np.all(cff.occ == [2,2,2,1,1])
+    assert np.all(cff.occ == [2, 2, 2, 1, 1])
+
 
 def test_CFF_energy_features():
     cff = CrystalFieldFeatures("cr", 3, 4)
     cff.occupy_d_orbitals()
-    assert abs(cff.calculate_enes(cff.occ, cff.geometry_diff_of_quanta_values["tetrahedral"]) - (-3.56)) <= EPS 
+    assert abs(cff.calculate_enes(cff.occ, cff.geometry_diff_of_quanta_values["tetrahedral"]) - (-3.56)) <= EPS
     cff = CrystalFieldFeatures("ni", 2, 3)
     cff.occupy_d_orbitals()
-    assert abs(cff.calculate_enes(cff.occ, cff.geometry_diff_of_quanta_values["square planar"]) - (-14.56)) <= EPS 
+    assert abs(cff.calculate_enes(cff.occ, cff.geometry_diff_of_quanta_values["square planar"]) - (-14.56)) <= EPS
     cff = CrystalFieldFeatures("mn", 3, 5)
     cff.occupy_d_orbitals()
-    assert abs(cff.calculate_enes(cff.occ, cff.geometry_diff_of_quanta_values["square planar"]) - (-12.28)) <= EPS 
+    assert abs(cff.calculate_enes(cff.occ, cff.geometry_diff_of_quanta_values["square planar"]) - (-12.28)) <= EPS
     cff = CrystalFieldFeatures("cr", 3, 2)
     cff.occupy_d_orbitals()
-    assert abs(cff.calculate_enes(cff.occ, cff.geometry_diff_of_quanta_values["tetrahedral"]) - (-8.01)) <= EPS 
+    assert abs(cff.calculate_enes(cff.occ, cff.geometry_diff_of_quanta_values["tetrahedral"]) - (-8.01)) <= EPS
     cff = CrystalFieldFeatures("fe", 2, 1)
     cff.occupy_d_orbitals()
-    assert abs(cff.calculate_enes(cff.occ, cff.geometry_diff_of_quanta_values["tetrahedral"]) - (-7.12)) <= EPS 
+    assert abs(cff.calculate_enes(cff.occ, cff.geometry_diff_of_quanta_values["tetrahedral"]) - (-7.12)) <= EPS
     cff = CrystalFieldFeatures("ni", 2, 1)
     cff.occupy_d_orbitals()
-    assert abs(cff.calculate_enes(cff.occ, cff.geometry_diff_of_quanta_values["square planar"]) - (-24.56)) <= EPS 
+    assert abs(cff.calculate_enes(cff.occ, cff.geometry_diff_of_quanta_values["square planar"]) - (-24.56)) <= EPS
     cff = CrystalFieldFeatures("cr", 2, 1)
     cff.occupy_d_orbitals()
-    assert abs(cff.calculate_enes(cff.occ, cff.geometry_diff_of_quanta_values["square planar"]) - (-20.56)) <= EPS 
+    assert abs(cff.calculate_enes(cff.occ, cff.geometry_diff_of_quanta_values["square planar"]) - (-20.56)) <= EPS
