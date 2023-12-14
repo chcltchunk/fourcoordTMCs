f808cdf743eab599b523a2780cdf979f265f6358
Jonas <joldenstaedt@gmail.com>
2023-12-14 21:03:55 +0100

    WIP: classifier features

--- fcTMCml/featurization.py
+++ fcTMCml/featurization.py
@@ -50,35 +50,49 @@ for i, row in df.iterrows():
     xyz_file_path = raw_data_dir + f"geometries/metal_{row['metal']}_ox_{int(row['ox'])}_spin_{int(row['spin'])}_ligstr_{row['ligstr']}.xyz"
     if i == 828:  # xyz_file_path == raw_data_dir + "geometries/metal_fe_ox_2_spin_1_ligstr_scn_furan_furan_furan.xyz":
         continue
-    mcdlf = MCDL53(graph_from_xyz_file(xyz_file_path), oxidation_state=int(row['ox']), ligand_list=row["ligstr"].split("_"),
+    ligand_list = row["ligstr"].split("_")
+    mcdlf = MCDL53(graph_from_xyz_file(xyz_file_path), oxidation_state=int(row['ox']), ligand_list=ligand_list,
                    multiplicity=row['spin'], input_file=xyz_file_path)
     cff = CrystalFieldFeatures(row["metal"], int(row['ox']), int(row['spin']))
-    rac = RAC(graph_from_xyz_file(xyz_file_path))
+    rac = RAC(graph_from_xyz_file(xyz_file_path), int(row['ox']), ligand_list=ligand_list)
     
     geometry_one_hot = 0 if row["geom"] == 'tetrahedral' else 1
     classifier_targets += [geometry_one_hot]
     f = mcdlf.get_classifier_features()
+    fn = mcdlf.get_classifier_feature_names()
     mcdl53_classifier_features += [f]
     f = mcdlf.get_classifier_features(additional_featurizer=[cff])
-    mcdl53_classifier_features += [f]
-    f = mcdlf.get_classifier_features()
-    mcdl53_classifier_features += [f]
-    f = mcdlf.get_classifier_features()
-    mcdl53_classifier_features += [f]
-    fn = mcdlf.get_classifier_feature_names()
+    mcdl53_cff_classifier_features += [f]
+    f = rac.get_classifier_features()
+    rac300_classifier_features += [f]
+    f = rac.get_classifier_features(additional_featurizer=[cff])
+    rac300_cff_classifier_features += [f]
     assert len(f) == len(fn)
-assert len(df) - 1 == len(mcdl53_classifier_features)
-print(classifier_targets)
 
-print(len(f), len(df), len(mcdl53_classifier_features))
-f = np.vstack(mcdl53_classifier_features)
-print(f.shape)
-# TODO:
-z_scores = StandardScaler()
+mcdl53_classifier_feature_names = mcdlf.get_classifier_feature_names()
+mcdl53_cff_classifier_feature_names = mcdlf.get_classifier_feature_names(additional_featurizer=[cff])
+rac300_classifier_feature_names = rac.get_classifier_feature_names()
+rac300_cff_classifier_feature_names = rac.get_classifier_feature_names(additional_featurizer=[cff])
 
 np.save(feature_target_dir + "classifier_targets.npy", np.array(classifier_targets))
 
+f = np.vstack(mcdl53_classifier_features)
+z_scores = StandardScaler()
+f = z_scores.fit_transform(f)
 np.save(feature_target_dir + "MCDL53_classifier.npy", f)
+
+f = np.vstack(mcdl53_cff_classifier_features)
+z_scores = StandardScaler()
+f = z_scores.fit_transform(f)
+np.save(feature_target_dir + "MCDL53_cff_classifier.npy", f)
+f = np.vstack(rac300_classifier_features)
+z_scores = StandardScaler()
+f = z_scores.fit_transform(f)
+np.save(feature_target_dir + "RAC_classifier.npy", f)
+f = np.vstack(rac300_cff_classifier_features)
+z_scores = StandardScaler()
+f = z_scores.fit_transform(f)
+np.save(feature_target_dir + "RAC_cff_classifier.npy", f)
 
 quit()
 
