c093f09b7c7886e8ede6becc0cd8b24a4a7065c8
Jonas <joldenstaedt@gmail.com>
2023-12-14 12:50:17 +0100

    cosmetics

--- tests/test_RAC.py
+++ tests/test_RAC.py
@@ -56,5 +56,5 @@ def test_get_tetrahedral_feature_names():
     rac = RAC(graph=get_dummy_graph())
     feature_names = rac.get_tetrahedral_feature_names()
     assert len(feature_names) == 300
-    feature_names = rac.get_tetrahedral_feature_names(depth=4, averaged=True)   
-    assert len(feature_names) == 150 
+    feature_names = rac.get_tetrahedral_feature_names(depth=4, averaged=True)
+    assert len(feature_names) == 150
