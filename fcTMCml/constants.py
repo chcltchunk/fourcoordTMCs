623cf3a13c195e6e1fe2c299bc7adc554b0f3a64
Jonas <joldenstaedt@gmail.com>
2024-01-07 15:25:35 +0100

    patched

--- fcTMCml/constants.py
+++ fcTMCml/constants.py
@@ -12,6 +12,10 @@ SSE_CUTOFF = -110
 
 # floating point accuracy for tests
 EPS = 1e-6
+test_resource_dir = "tests/testresources/"
+raw_data_dir = "data/"
+feature_target_dir = "fcTMCml/features/"
+cache_dir = "cache/"
 
 # define properties we'd like to keep in the dataset throughout the complete workflow
 # TODO(ralf): should we have the formal charge in the final dataset?
