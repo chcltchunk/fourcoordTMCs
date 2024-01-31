f981a806d4b7b706c1adc6ccc90d36b54f8474dc
Jonas Oldenstaedt <joldenstaedt@gmail.com>
2024-01-31 14:01:52 +0100

    k_fold hyperparameter search

--- fcTMCml/sse_regression.py
+++ fcTMCml/sse_regression.py
@@ -20,11 +20,12 @@ from fcTMCml.tools import load_features
 
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
+from tensorflow.keras.layers import Dense, Dropout, BatchNormalization, Add
 from tensorflow.keras.optimizers.legacy import Adam, SGD, RMSprop, Adagrad, Adadelta, Nadam  
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.models import model_from_json, load_model
 from tensorflow import keras
+from tensorflow.keras.utils import plot_model
 from tensorflow.keras import initializers, regularizers
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -49,15 +50,15 @@ if gpus:
 
 
 space = {
-    'optimizer': hp.choice('optimizer', [Adam, SGD]),
-    'epochs': hp.choice('epochs', [300, 800]),
-    'batch_size': hp.choice('batch_size',  [256]), # [16, 64]),
-    'learning_rate': hp.loguniform('learning_rate', -6, -2),
-    'num_layers': hp.choice('layers', [4]),
+    'optimizer': hp.choice('optimizer', [Adam]),
+    'epochs': hp.choice('epochs', [150, 300, 800]),
+    'batch_size': hp.choice('batch_size', [16, 32, 256]),  # [16, 64]),
+    'learning_rate': hp.loguniform('learning_rate', -5, -1),
+    'num_layers': hp.choice('layers', [2, 3]),
     'layers': [
         {
             'type': 'dense',
-            'units': hp.choice('dense_units', [64, 128, 200]),
+            'units': hp.choice('dense_units', [64, 128]),
             'activation': hp.choice('dense_activation', ['leaky_relu', 'elu']),
             'l2_reg': hp.loguniform('dense_l2_reg', -3, 0),
             'batch_norm' : hp.uniform('batch_norm', 0.85, 0.99),
@@ -114,30 +115,33 @@ def create_model(params):
                 model.add(BatchNormalization(momentum=layer_params['batch_norm']))
 
     model.add(Dense(1, activation='linear'))
-    model.compile(optimizer=params['optimizer'](learning_rate=params['learning_rate']), loss='mae', metrics=['mae', 'mse'])
+    model.compile(optimizer=params['optimizer'](learning_rate=params['learning_rate']), loss='mse', metrics=['mae', 'mse'])
     # print(model.summary())
     return model
 
 
-def objective(params):
+def training(params, key: str = "RAC_cff_regression", return_history: bool = False):
     model = create_model(params)
     # Train your model and return the value to minimize
     X_train, Y_train, X_test, Y_test = data()
-    es = EarlyStopping(monitor='mae', mode='auto', verbose=0, patience=100)
+    es = EarlyStopping(monitor='mse', mode='auto', verbose=0, patience=100)
     # print(X_train.shape, Y_train.shape)
     # quit()
     history = model.fit(X_train, Y_train, validation_data=(X_test, Y_test), epochs=params['epochs'], batch_size=params['batch_size'], verbose=1, callbacks=[es])
-    loss = history.history['val_mae'][-1]
+    loss = history.history['val_mse'][-1]
+    # plot_model(model, "model.png")
+    if return_history:
+        return history
     return loss
 
 
-def data():
+def data(key: str = "RAC_cff_regression"):
     # import data
     regression_in_subdir = "regression_rff_selection/"
 
     regression_targets, feature_dict, feature_names_dict = load_features(feature_target_dir, regression_in_subdir, "regression")
 
-    X = feature_dict["RAC_cff_regression"]
+    X = feature_dict[key]
     y_truth = regression_targets
     # scaler = StandardScaler()
     # y_truth = scaler.fit_transform(regression_targets.reshape(-1, 1))
@@ -148,17 +152,17 @@ def data():
 
     # X = X[mask, :]
     # y_truth = y_truth[mask]
-    X_train, X_test, Y_train, Y_test = train_test_split(X, y_truth, test_size=0.1, random_state=0)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, y_truth, test_size=0.1, random_state=821)
     return X_train, Y_train.reshape(-1, 1), X_test, Y_test.reshape(-1, 1)
 
 
-def run_kfold(params):
+def run_kfold(params, key: str = "RAC_cff_regression"):
     # import data
     regression_in_subdir = "regression_rff_selection/"
 
     regression_targets, feature_dict, feature_names_dict = load_features(feature_target_dir, regression_in_subdir, "regression")
 
-    X = feature_dict["RAC_cff_regression"]
+    X = feature_dict[key]
     y_truth = regression_targets.reshape(-1, 1)
 
     kf = KFold(n_splits=10, shuffle=True, random_state=0)
@@ -170,17 +174,32 @@ def run_kfold(params):
         Y_train, Y_test = y_truth[train_index], y_truth[test_index]
 
         model = create_model(params)
-        es = EarlyStopping(monitor='mae', mode='auto', verbose=0, patience=100)
+        es = EarlyStopping(monitor='mse', mode='auto', verbose=0, patience=100)
     
-        history = model.fit(X_train, Y_train, validation_data=(X_test, Y_test), epochs=params['epochs'], batch_size=params['batch_size'], verbose=1, callbacks=[es])
+        history = model.fit(X_train, Y_train, validation_data=(X_test, Y_test), epochs=params['epochs'],
+                            batch_size=params['batch_size'], verbose=1, callbacks=[es])
         loss = history.history['val_mae'][-1]
         loss_n += [loss]
         loss = history.history['mae'][-1]
         loss_train_n += [loss]
     return np.average(loss_train_n), np.average(loss_n)
-    
-def objective_kfold(params):
+
+
+def objective_rac_cff(params):
+    return training(params)
+
+
+def objective_rac(params):
+    return training(params, key="RAC_regression")
+
+
+def objective_kfold_rac_cff(params):
     _, b = run_kfold(params)
+    return b
+
+
+def objective_kfold_rac(params):
+    _, b = run_kfold(params, key="RAC_regression")
     return b
 
 
@@ -191,12 +210,32 @@ keras.utils.set_random_seed(821)
 if __name__ == "__main__":
     import gc
     gc.collect()
+    result_dict = {}
     with K.get_session():
-        best = fmin(fn=objective, space=space, algo=tpe.suggest, max_evals=30, show_progressbar=True)
+        best = fmin(fn=objective_kfold_rac_cff, space=space, algo=tpe.suggest, max_evals=400, show_progressbar=True)
         print("Best hyperparameters:", best)
         # best = {'batch_norm': 0.9346860946367082, 'batch_norm_1': 0.9702570581854131, 'batch_norm_2': 0.9249498413567074, 'batch_norm_3': 0.9284334406266748, 'batch_size': 0, 'dense_activation': 1, 'dense_activation_1': 0, 'dense_activation_2': 1, 'dense_activation_3': 1, 'dense_dropout': 0.1810803373583213, 'dense_dropout_1': 0.49460993462692243, 'dense_dropout_2': 0.15088107232837267, 'dense_dropout_3': 0.42973749770328307, 'dense_l2_reg': 0.10607279197122027, 'dense_l2_reg_1': 0.06232231061813992, 'dense_l2_reg_2': 0.1140963417907163, 'dense_l2_reg_3': 0.9067263774921112, 'dense_units': 1, 'dense_units_1': 0, 'dense_units_2': 1, 'dense_units_3': 0, 'epochs': 0, 'layers': 0, 'learning_rate': 0.090201604561948, 'optimizer': 1}
         best_hp = space_eval(space, best)
-        train_loss, val_loss = run_kfold(best_hp)
-        print(train_loss, val_loss)
+        history = training(best_hp, key="RAC_cff_regression", return_history=True)
+        train_loss, val_loss = history.history["mae"][-1], history.history["val_mae"][-1]
+        np.save("train_mae_rac_cff.npy", history.history["mae"])
+        np.save("val_mae_rac_cff.npy", history.history["val_mae"])
+        result_dict["RAC_cff_regression"] = [train_loss, val_loss, best_hp]
+        # train_loss, val_loss = run_kfold(best_hp)
+        # print(train_loss, val_loss)
+        # result_dict["RAC_cff_regression_kfold"] = [train_loss, val_loss, best_hp]
         
-
+        best = fmin(fn=objective_kfold_rac, space=space, algo=tpe.suggest, max_evals=400, show_progressbar=True)
+        print("Best hyperparameters:", best)
+        # best = {'batch_norm': 0.9346860946367082, 'batch_norm_1': 0.9702570581854131, 'batch_norm_2': 0.9249498413567074, 'batch_norm_3': 0.9284334406266748, 'batch_size': 0, 'dense_activation': 1, 'dense_activation_1': 0, 'dense_activation_2': 1, 'dense_activation_3': 1, 'dense_dropout': 0.1810803373583213, 'dense_dropout_1': 0.49460993462692243, 'dense_dropout_2': 0.15088107232837267, 'dense_dropout_3': 0.42973749770328307, 'dense_l2_reg': 0.10607279197122027, 'dense_l2_reg_1': 0.06232231061813992, 'dense_l2_reg_2': 0.1140963417907163, 'dense_l2_reg_3': 0.9067263774921112, 'dense_units': 1, 'dense_units_1': 0, 'dense_units_2': 1, 'dense_units_3': 0, 'epochs': 0, 'layers': 0, 'learning_rate': 0.090201604561948, 'optimizer': 1}
+        best_hp = space_eval(space, best)
+        history = training(best_hp, key="RAC_regression", return_history=True)
+        train_loss, val_loss = history.history["mae"][-1], history.history["val_mae"][-1]
+        np.save("train_mae_rac.npy", history.history["mae"])
+        np.save("val_mae_rac.npy", history.history["val_mae"])
+        result_dict["RAC_regression"] = [train_loss, val_loss, best_hp]
+        # train_loss, val_loss = run_kfold(best_hp)
+        # print(train_loss, val_loss)
+        # result_dict["RAC_regression_kfold"] = [train_loss, val_loss, best_hp]
+    print(result_dict)
+        
